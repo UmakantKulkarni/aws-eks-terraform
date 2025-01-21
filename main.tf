d4d4fc48c57fc0ef8fedb67badda7cda46750369
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2025-01-21 01:28:47 -0500

    custom ami - not working

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "5gc-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -23,11 +23,36 @@ resource "random_string" "suffix" {
   special = false
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = module.vpc.vpc_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = module.vpc.public_subnets[0]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
+  version = "5.17.0"
 
-  name = "education-vpc"
+  name = "5gc-vpc"
 
   cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
@@ -35,9 +60,9 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = false
   enable_dns_hostnames = true
+  create_igw           = false
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
@@ -45,16 +70,62 @@ module "vpc" {
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
   }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  count          = length(module.vpc.private_subnets)
+  subnet_id      = element(module.vpc.private_subnets, count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_iam_role" "custom_eks_role" {
+  name               = "custom-eks-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "eks_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "custom_eks_role_attachments" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+  ])
+
+  role       = aws_iam_role.custom_eks_role.name
+  policy_arn = each.value
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
+  version = "20.33.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.29"
-
+  cluster_name                             = local.cluster_name
+  cluster_version                          = "1.31"
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
@@ -67,30 +138,19 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
+    custom-group = {
+      name           = "custom-node-group"
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 2
       instance_types = ["t3.small"]
+      iam_role_arn   = aws_iam_role.custom_eks_role.arn
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      launch_template_id      = aws_launch_template.custom_eks_lt.id
+      launch_template_version = aws_launch_template.custom_eks_lt.latest_version
+      ami_id                  = var.custom_ami_id
+      ami_type                = "CUSTOM"
     }
   }
 }
@@ -103,11 +163,71 @@ data "aws_iam_policy" "ebs_csi_policy" {
 
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
+  version = "5.52.2"
 
   create_role                   = true
   role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_launch_template" "custom_eks_lt" {
+  name_prefix   = "eks-custom-lt"
+  description   = "Launch template for custom EKS AMI"
+  image_id      = var.custom_ami_id
+  instance_type = var.instance_type
+
+  key_name = var.ssh_key_name # SSH key for remote access
+
+  network_interfaces {
+    associate_public_ip_address = true
+    delete_on_termination       = true
+  }
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    set -o xtrace
+    /etc/eks/bootstrap.sh ${local.cluster_name}
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "eks-node"
+      "kubernetes.io/cluster/${local.cluster_name}" = "owned"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Allow access from all IPs (restrict this in production)
+  security_group_id = module.eks.node_security_group_id
+}
+
+resource "aws_security_group_rule" "eks_control_plane_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Replace with specific source if needed
+  security_group_id = module.eks.node_security_group_id
+}
+
+resource "aws_security_group_rule" "eks_cluster_ingress" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = [module.vpc.vpc_cidr_block]
+  security_group_id = module.eks.node_security_group_id
 }
--- variables.tf
+++ variables.tf
@@ -4,5 +4,23 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
+}
+
+variable "custom_ami_id" {
+  description = "Custom AMI ID for EKS nodes - https://cloud-images.ubuntu.com/aws-eks/"
+  type        = string
+  default     = "ami-0689d81543aa65690"
+}
+
+variable "instance_type" {
+  description = "Instance type for EKS nodes"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "ssh_key_name" {
+  description = "Name of the SSH key pair for accessing nodes"
+  type        = string
+  default     = "ec2kp1"
 }
